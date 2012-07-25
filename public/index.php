eb7daeecb6ecc62408622cf5996daf229f68879c
Y.Nishimura <nishimuray@gmail.com>
2012-07-26 03:12:34 +0900

    phpcsでコメント部にエラーが出ないようにしてみた

--- public/index.php
+++ public/index.php
@@ -1,8 +1,33 @@
 <?php
 /**
- * test
+ * コメントテスト
  *
- * @author Y.Nishimura
+ * 後でファイル名変えると思う。
+ * Docコメントのテスト中
+ * <pre>
+ * メモ：
+ * ・PHPのバージョン表記
+ * ・カテゴリ、パッケージには順序がある
+ * ・ライセンス、リンクには順序がある
+ * ・authorは名前とメールアドレスが必須
+ * ・カテゴリ、パッケージは先頭大文字
+ * </pre>
+ *
+ * PHP version 5.3
+ *
+ * @category  OfficeL
+ * @package   Officel
+ * @author    Y.Nishimura <nishimuray@gmail.com>
+ * @copyright 2012 - Office L
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
+ * @link      http://github.com/officel/jenkins-php-test.git
+ */
+/**
+ * works
+ *
+ * this is work
+ *
+ * @author "Y.Nishimura"
  */
 
 echo "OK";
