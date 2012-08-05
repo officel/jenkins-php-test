e200117ac3b1ab14ab08b6f96c8398e747f7ff6f
Y.Nishimura <nishimuray@gmail.com>
2012-08-06 01:18:33 +0900

    phpcs対応。ファイルコメント、クラスコメントの追加と、メソッドの前後に空白行を入れる。メソッド名の丸括弧とブラケットの間にスペースをひとついれる。

--- tests/jenkins-php-testTest.php
+++ tests/jenkins-php-testTest.php
@@ -1,17 +1,47 @@
 <?php
+/**
+ * PHP Unit Test test
+ *
+ * PHP　version 5.3
+ *
+ * @category  OfficeL
+ * @package   Officel
+ * @author	Y.Nishimura <nishimuray@gmail.com>
+ * @copyright 2012 - Office L
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
+ * @link	  http://github.com/officel/jenkins-php-test.git
+ */
+/**
+ * PHP　Unit Test test 1st test
+ *
+ * this is test now.
+ *
+ * @category  OfficeL
+ * @package   Officel
+ * @author	Y.Nishimura <nishimuray@gmail.com>
+ * @copyright 2012 - Office L
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
+ * @link	  http://github.com/officel/jenkins-php-test.git
+ */
 class JenkinsPhpTestTest extends PHPUnit_Framework_TestCase
 {
-    public function testPushAndPop()
-    {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
+
+
+	/**
+	 * test 1st 
+	 */
+	public function testPushAndPop() {
+		$stack = array();
+		$this->assertEquals(0, count($stack));
  
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
+		array_push($stack, 'foo');
+		$this->assertEquals('foo', $stack[count($stack) - 1]);
+		$this->assertEquals(1, count($stack));
  
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
-    }
+		$this->assertEquals('foo', array_pop($stack));
+		$this->assertEquals(0, count($stack));
+	}
+
+
 }
 
